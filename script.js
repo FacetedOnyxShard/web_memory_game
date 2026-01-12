3699eeab7322dbf77d472ae27aaa24232bc6bfcc
hikaruvi <daniilrybkin32@gmail.com>
2026-01-12 16:43:44 +0700

    add cards rotation

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const cardsMap = document.getElementById("map");
+
+const cardsInGridRow = 4;
+
+for (let i = 0; i < cardsInGridRow ** 2; ++i) {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  cardsMap.appendChild(card);
+}
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("active");
+  });
+});
